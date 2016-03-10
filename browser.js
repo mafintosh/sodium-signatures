5c5c35c7b5e36bf74228817ef886b37a54167c2e
Mathias Buus <mathiasbuus@gmail.com>
2016-03-10 11:01:07 -0800

    buffers are uint8 arrays now

--- browser.js
+++ browser.js
@@ -1,28 +1,16 @@
 var tweetnacl = require('tweetnacl')
 
 exports.keyPair = function () {
-  var publicKey = new Buffer(tweetnacl.lowlevel.crypto_sign_PUBLICKEYBYTES)
-  var secretKey = new Buffer(tweetnacl.lowlevel.crypto_sign_SECRETKEYBYTES)
+  var publicKey = Buffer(tweetnacl.lowlevel.crypto_sign_PUBLICKEYBYTES)
+  var secretKey = Buffer(tweetnacl.lowlevel.crypto_sign_SECRETKEYBYTES)
   tweetnacl.lowlevel.crypto_sign_keypair(publicKey, secretKey)
   return {publicKey: publicKey, secretKey: secretKey}
 }
 
 exports.verify = function (message, signature, publicKey) {
-  return tweetnacl.sign.detached.verify(toUint8Array(message), toUint8Array(signature), toUint8Array(publicKey))
+  return tweetnacl.sign.detached.verify(message, signature, publicKey)
 }
 
 exports.sign = function (message, secretKey) {
-  return toBuffer(tweetnacl.sign.detached(toUint8Array(message), toUint8Array(secretKey)))
-}
-
-function toUint8Array (message) {
-  var message8 = new Uint8Array(message.length)
-  for (var i = 0; i < message8.length; i++) message8[i] = message[i]
-  return message8
-}
-
-function toBuffer (message8) {
-  var message = new Buffer(message8.length)
-  for (var i = 0; i < message.length; i++) message[i] = message8[i]
-  return message
+  return Buffer(tweetnacl.sign.detached(message, secretKey))
 }
