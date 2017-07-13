7b68f4baa361b30da37781ae14dc7895ccd8d004
Mathias Buus <mathiasbuus@gmail.com>
2017-07-13 17:18:14 +0200

    use sodium-universal

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-try {
-  module.exports = require('./sodium')
-} catch (err) {
-  module.exports = require('./browser')
+var sodium = require('sodium-universal')
+
+exports.keyPair = function (seed) {
+  var publicKey = new Buffer(sodium.crypto_sign_PUBLICKEYBYTES)
+  var secretKey = new Buffer(sodium.crypto_sign_SECRETKEYBYTES)
+
+  if (seed) sodium.crypto_sign_seed_keypair(publicKey, secretKey, seed)
+  else sodium.crypto_sign_keypair(publicKey, secretKey)
+
+  return {
+    publicKey: publicKey,
+    secretKey: secretKey
+  }
+}
+
+exports.sign = function (message, secretKey) {
+  var signature = new Buffer(sodium.crypto_sign_BYTES)
+  sodium.crypto_sign_detached(signature, message, secretKey)
+  return signature
+}
+
+exports.verify = function (message, signature, publicKey) {
+  return sodium.crypto_sign_verify_detached(signature, message, publicKey)
 }
